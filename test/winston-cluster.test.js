2afd598f3fc27102225a9e5e97985316fef774f4
Ryan Kurte <ryan.kurte@trutest.co.nz>
2015-06-26 11:41:10 +1200

    No idea how to test (yet).

--- test/winston-cluster.test.js
+++ test/winston-cluster.test.js
@@ -3,8 +3,18 @@ var winston = require('winston');
 
 var winstonCluster = require('../lib/winston-cluster');
 
+//TODO: how to test with clustering ..?
+
 //Datastore testing
 describe('Winston Cluster Tests', function() {
+
+	beforeEach(function(done) {
+
+	});
+
+	afterEach(function(done) {
+
+	});
 
     it.skip("can bind to cluster threads", function(done) {
         done();
