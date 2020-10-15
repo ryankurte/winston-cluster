4d87a88adf32e5aadf5bea138b658dd2550be01f
Christoffer Nørskov <christoffer.noerskov@ssi-schaefer.com>
2020-10-15 13:36:58 +0200

    Add static masterLogger, allows for multiple logger instances on master thread with only one instance logging to console or file.

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,17 @@ import * as cluster from 'cluster';
 
 declare namespace winstonCluster {
   interface Cluster extends transportStream {
+    masterLogger: winston.Logger;
+
     name: string;
     silent: boolean;
     stripColors: boolean;
     loggerName: string;
 
-    bindListeners(instance: any): void;
-    bindListener(worker: cluster.Worker, instance: any): void;
-    bindMultipleListeners(instances: any[]): void;
+    bindListeners(instance: winston.Logger): void;
+    bindListener(worker: cluster.Worker, instance: winston.Logger): void;
+    bindMultipleListeners(instances: winston.Logger[]): void;
+    setMasterLogger(instance: winston.Logger): void;
     log(info: any, callback: Function): void;
 
     new(opts: transportStream.TransportStreamOptions, loggerName?: string): Cluster;
